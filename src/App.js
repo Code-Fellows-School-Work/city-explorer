7c53e0e8e03a80595983ca24cc3dfcb319ed0860
Errol Vidad <errolvidad@gmail.com>
2023-12-07 05:13:23 +0000

    troubleshooting why retrived sever data will not render city forecast to webpage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// this entire component is used from John's class demo with the exception of ErrorCode
+// Inside your App.js file
 
 import { useState } from 'react';
 import axios from 'axios';
@@ -7,22 +7,24 @@ import CityForm from "./components/CityForm.jsx";
 import Map from './components/Map.jsx';
 import CityInfo from './components/CityInfo.jsx';
 import ErrorDisplay from './components/ErrorCode.jsx';
+import Weather from './components/Weather';
 import './App.css';
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 function App() {
-  console.log("API Key:", API_KEY);
+  // console.log("API Key:", API_KEY);
 
   const [city, setCity] = useState('');
   const [latitude, setLatitude] = useState(null);
   const [longitude, setLongitude] = useState(null);
   const [error, setError] = useState(null);
+  const [forecast, setForecast] = useState(null);
 
   // New function to fetch weather data
   async function getWeatherData(lat, lon, searchQuery) {
     try {
-      let weatherResponse = await axios.get(`http://localhost:5505/weather`, {
+      let weatherResponse = await axios.get(`http://localhost:5510/weather`, {
         params: {
           lat: lat,
           lon: lon,
@@ -30,9 +32,13 @@ function App() {
         },
       });
 
+      // Log the entire response for debugging
+      console.log('Weather Response:', weatherResponse.data);
+
       // Access the city and forecast data from the response
       const cityData = weatherResponse.data.city;
       const forecastData = weatherResponse.data.forecast;
+      setForecast(forecastData);
 
       // Do something with the city and forecast data (e.g., update state)
       console.log('City Data:', cityData);
@@ -60,7 +66,11 @@ function App() {
       setError(null);
 
       // 5. Make a new request to the /weather endpoint
-      getWeatherData(response.data[0].lat, response.data[0].lon, cityName);
+      getWeatherData(
+        parseFloat(response.data[0].lat).toFixed(2),
+        parseFloat(response.data[0].lon).toFixed(2),
+        cityName
+      );
     } catch (error) {
       setError(error.response ? error.response.status : 500);
     }
@@ -84,9 +94,9 @@ function App() {
         <Map latitude={latitude} longitude={longitude} />
       )}
       {city && <CityInfo cityName={city} latitude={latitude} longitude={longitude} />}
+      {forecast && <Weather forecastData={forecast} />}
     </div>
   );
-};
+}
 
 export default App;
-
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,1 +1,22 @@
+// used ChatGPT to write this component
+
 import React from 'react';
+
+function Weather({ forecastData }) {
+  return (
+    <div>
+      <h2>Weather Forecast for {forecastData.city_name}</h2>
+      {forecastData.city && forecastData.city.length > 0 ? (
+        <div>
+          <p>Date: {forecastData.city[0].valid_date}</p>
+          <p>Description: {forecastData.city[0].weather.description}</p>
+          {/* Add other weather details as needed */}
+        </div>
+      ) : (
+        <p>No forecast data available</p>
+      )}
+    </div>
+  );
+}
+
+export default Weather;
