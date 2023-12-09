40d6d7c6d51c0b26677a8266128a6cfc520acbd6
Errol Vidad <errolvidad@gmail.com>
2023-12-09 23:44:11 +0000

    moved api calls to server

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+// App.js
 import { useState } from 'react';
 import axios from 'axios';
-import Header from "./components/Header.jsx";
-import CityForm from "./components/CityForm.jsx";
+import Header from './components/Header.jsx';
+import CityForm from './components/CityForm.jsx';
 import Map from './components/Map.jsx';
 import CityInfo from './components/CityInfo.jsx';
 import ErrorDisplay from './components/ErrorCode.jsx';
@@ -9,28 +10,17 @@ import Weather from './components/Weather.jsx';
 import Movies from './components/Movies.jsx';
 import './App.css';
 
-const API_KEY = process.env.REACT_APP_API_KEY;
-const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
-const MOVIE_API_KEY = process.env.REACT_APP_MOVIE_API_KEY;
-
 function App() {
-  // console.log("API Key:", API_KEY);
-
   const [city, setCity] = useState('');
   const [latitude, setLatitude] = useState(null);
   const [longitude, setLongitude] = useState(null);
   const [error, setError] = useState(null);
-  // changed to [] instead of null? -- I changed state to an empty array then modified weather.jsx file to use map and it sucessfully rendered the server weather data onto the webpage
-  // const [forecast, setForecast] = useState([]);
   const [weatherData, setWeatherData] = useState(null);
   const [moviesData, setMoviesData] = useState([]);
 
   async function getWeatherData(lat, lon) {
     try {
-      const weatherResponse = await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${WEATHER_API_KEY}&days=2`);
-      console.log('Weather Response:', weatherResponse.data);
-
-      // Set the weather data in state
+      const weatherResponse = await axios.get(`http://localhost:5513/api/weather?lat=${lat}&lon=${lon}`);
       setWeatherData(weatherResponse.data);
     } catch (error) {
       console.error('Error fetching weather data:', error);
@@ -38,8 +28,25 @@ function App() {
     }
   }
 
+  async function getMoviesData(cityName) {
+    try {
+      const moviesResponse = await axios.get(`http://localhost:5513/api/movies?cityName=${cityName}`);
+      setMoviesData(moviesResponse.data);
+    } catch (error) {
+      console.error('Error fetching movie data:', error);
+      setError('Error fetching movie data');
+    }
+  }
+
+  function changeCity(newCity) {
+    getLocation(newCity);
+    console.log('Changing to', newCity);
+
+    getMoviesData(newCity);
+  }
+
   async function getLocation(cityName) {
-    const locationUrl = `https://us1.locationiq.com/v1/search?key=${API_KEY}&q=${cityName}&format=json`;
+    const locationUrl = `http://localhost:5513/api/location?cityName=${cityName}`;
 
     try {
       const locationResponse = await axios.get(locationUrl);
@@ -57,28 +64,6 @@ function App() {
     }
   }
 
-  async function getMoviesData(cityName) {
-    try {
-      const moviesResponse = await axios.get(
-        `https://api.themoviedb.org/3/search/movie?query=${cityName}&api_key=${MOVIE_API_KEY}`
-      );
-  
-      console.log('Movies Response:', moviesResponse.data);
-  
-      setMoviesData(moviesResponse.data.results);
-    } catch (error) {
-      console.error('Error fetching movie data:', error);
-      setError('Error fetching movie data');
-    }
-  }
-
-  function changeCity(newCity) {
-    getLocation(newCity);
-    console.log("Changing to", newCity);
-
-    getMoviesData(newCity);
-  }
-
   return (
     <div>
       <Header />
@@ -90,7 +75,7 @@ function App() {
           <Map latitude={latitude} longitude={longitude} />
           {city && <CityInfo cityName={city} latitude={latitude} longitude={longitude} />}
           {weatherData && <Weather weatherData={weatherData} />}
-          {moviesData ? <Movies moviesData={moviesData} error={error} /> : <p>Hello World</p>} 
+          {moviesData ? <Movies moviesData={moviesData} error={error} /> : <p>Hello World</p>}
         </>
       )}
     </div>
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,27 +1,14 @@
 // this entire component is used from John's class demo
-
-import {When} from 'react-if';
-
-// Read in from .env.local file and live, it comes from "Environment Variables"
-const API_KEY = process.env.REACT_APP_API_KEY;
+import { When } from 'react-if';
 
 function Map(props) {
-
-  // Lat and Lon from props ...
-  // Key from the .env
-  // Build a URL
-
-  // https://maps.locationiq.com/v3/staticmap?key=<YOUR_ACCESS_TOKEN>&center=<latitude>,<longitude>&zoom=<zoom>&size=<width>x<height>&format=<format>&maptype=<MapType>&markers=icon:<icon>|<latitude>,<longitude>&markers=icon:<icon>|<latitude>,<longitude>
-
-
-   // { this ? that : somethingElse }
   return (
     <When condition={props.latitude && props.longitude}>
       <figure>
-        <img src={`https://maps.locationiq.com/v3/staticmap?key=${API_KEY}&center=${props.latitude},${props.longitude}&size=600x600&format=png`} alt="" />
+        <img src={`http://localhost:5513/api/map?lat=${props.latitude}&lon=${props.longitude}`} alt="" />
       </figure>
     </When>
-  )
+  );
 }
 
 export default Map;
